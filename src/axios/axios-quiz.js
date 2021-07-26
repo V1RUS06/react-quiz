95a784ff80b99b2597d77cace65d743f8c58d0c2
Denis <denison4@mail.ru>
2021-07-26 16:07:40 +0300

    first-commit

---
+++ src/axios/axios-quiz.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+    // baseURL: 'https://reat-quiz-f9e45-default-rtdb.firebaseio.com/'
+    baseURL: 'https://quiz-ec0f0-default-rtdb.firebaseio.com/'
+})
